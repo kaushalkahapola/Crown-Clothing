e318dcd0d7eacac867f9fe0c104ea1d7ec223219
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-02-10 13:45:00 +0530

    created project and added files for homepage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import HomePage from './pages/HomePage/HomePage.component'
+
+function App() {
+  return (
+    <>
+      <div>
+        <HomePage></HomePage>
+      </div>
+    </>
+  )
+}
+
+export default App
