ecf1d7f36d5ab1a69647cc445a7251667b0755d4
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-02-10 15:12:10 +0530

    broken the homepage into components, styled the menu-items

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -1,41 +1,11 @@
 import React from "react";
 import "./HomePage.styles.scss";
+import Directory from "../../components/Directory/Directory.component";
 
 const HomePage = () =>{
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">JACKETS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">SNEAKERS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">WOMEN</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">MEN</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+            <Directory />
         </div>
     )
 }
