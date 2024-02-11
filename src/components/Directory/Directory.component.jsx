d862eb28b80950f353d9e7a04719341daa0b885f
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-02-11 22:29:42 +0530

    images added

--- src/components/Directory/Directory.component.jsx
+++ src/components/Directory/Directory.component.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { MenuItem } from "../MenuItem/MenuItem.component";
 import "./Directory.styles.scss";
+import hats from '../../assets/images/hats.png'
+import jackets from '../../assets/images/jackets.png'
+import sneakers from '../../assets/images/sneakers.png'
+import womens from '../../assets/images/womens.png'
+import men from '../../assets/images/men.png'
 
 class Directory extends React.Component{
     constructor(){
@@ -9,32 +14,32 @@ class Directory extends React.Component{
             sections: [
                 {
                   title: 'Hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  imageUrl: hats,
                   id: 1,
                   linkUrl: 'hats'
                 },
                 {
                   title: 'Jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  imageUrl: jackets,
                   id: 2,
                   linkUrl: ''
                 },
                 {
                   title: 'Sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  imageUrl: sneakers,
                   id: 3,
                   linkUrl: ''
                 },
                 {
                   title: 'Womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  imageUrl: womens,
                   size: 'large',
                   id: 4,
                   linkUrl: ''
                 },
                 {
                   title: 'Mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  imageUrl: men,
                   size: 'large',
                   id: 5,
                   linkUrl: ''
